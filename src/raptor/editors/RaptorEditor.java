3c54d34e18d453690682cd5b706cf2d00a4c17b6
puma <raphaeldjondo@gmail.com>
2011-01-30 12:31:34 -0500

    First commit of files

---
+++ src/raptor/editors/RaptorEditor.java
@@ -0,0 +1,45 @@
+package raptor.editors;
+
+
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.*;
+import org.eclipse.ui.part.MultiPageEditorPart;
+import org.eclipse.ui.part.FileEditorInput;
+
+public class RaptorEditor extends MultiPageEditorPart implements IResourceChangeListener{
+
+  @Override
+  public void resourceChanged(IResourceChangeEvent arg0) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  protected void createPages() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void doSave(IProgressMonitor arg0) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void doSaveAs() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public boolean isSaveAsAllowed() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  
+  
+}
